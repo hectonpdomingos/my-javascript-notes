41c8c74104cf1be7bd429969abb19fce898f2e77
Hecton <hecton@localhost.localdomain>
2019-06-11 19:18:24 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry : [
+            'babel-polyfill', './src/index'
+        ],
+   output: {
+       path: __dirname + '/public/',
+       filename: 'bundle.js',
+   },
+   devServer:{
+       contentBase: __dirname + '/public'
+   },
+   module: {
+       rules : [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+            }
+       ],
+   }, 
+};
